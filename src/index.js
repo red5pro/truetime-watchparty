b9c48839f6c374216211062ff3bcdd1e352d2e08
LourdesBl <bloin.lourdes@gmail.com>
2022-07-14 17:22:49 -0300

    create app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
-import AppRoutes from './routes/routes'
+import AppRoutes from './routes/Routes'
 import { Typography } from '@mui/material'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
