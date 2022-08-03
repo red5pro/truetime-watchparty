56b7cfb13ace4cfd469fbd5d6d21666535c0c2f5
LourdesBl <bloin.lourdes@gmail.com>
2022-08-03 18:18:07 -0300

    feature/WAT-64-landing-page

--- src/pages/Home/Home.module.ts
+++ src/pages/Home/Home.module.ts
@@ -1,15 +1,32 @@
 import { makeStyles } from 'tss-react/mui'
 
-const useStyles = makeStyles()(() => {
+const useStyles = makeStyles()((theme: any) => {
   return {
     container: {
-      margin: '1rem',
-      display: 'flex',
-      flexDirection: 'column',
+      height: '100vh',
+      [theme.breakpoints.down('sm')]: {
+        width: '100%',
+        height: 'auto',
+        flexDirection: 'column',
+      },
     },
     leftContainer: {
       background: 'linear-gradient(90deg, #1B1828 0%, rgba(20, 20, 20, 0.4) 40%)',
       backdropFilter: 'blur(88px)',
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      alignItems: 'flex-start',
+      justifyContent: 'center',
+      marginLeft: '15%',
+      width: '60%',
+
+      [theme.breakpoints.down('sm')]: {
+        width: '100%',
+      },
+    },
+    titleContainer: {
+      margin: '2.5rem 0',
     },
     title: {
       fontSize: '90px',
@@ -26,6 +43,32 @@ const useStyles = makeStyles()(() => {
       fontSize: '18px',
       fontWeight: 600,
       lineHeight: '25.65px',
+    },
+    input: {},
+    link: {
+      fontWeight: 600,
+    },
+    rightContainer: {
+      width: '40%',
+
+      [theme.breakpoints.down('sm')]: {
+        width: '100%',
+        height: 'auto',
+      },
+    },
+    imageContainer: {
+      width: '100%',
+      height: '100vh',
+      [theme.breakpoints.down('sm')]: {
+        width: '100%',
+        height: 'auto',
+      },
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      objectPosition: 'right',
     },
   }
 })
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,42 @@
 import { Box, Input, Typography } from '@mui/material'
 import WbcLogo from '../../assets/logos/WbcLogo'
-import CustomButton, { BUTTONTYPE } from '../../components/Common/CustomButton/CustomButton'
-// import { Link } from 'react-router-dom'
+import CustomButton, { BUTTONSIZE, BUTTONTYPE } from '../../components/Common/CustomButton/CustomButton'
+import { Link } from 'react-router-dom'
 import useStyles from './Home.module'
 
 const Home = () => {
   const { classes } = useStyles()
   return (
-    <Box className={classes.container} display="flex" flexDirection="column">
-      <Box width="60%" className={classes.leftContainer}>
+    <Box className={classes.container} display="flex">
+      <Box className={classes.leftContainer}>
         <WbcLogo />
-        <Typography className={classes.title}>Watch Party</Typography>
-        <Typography className={classes.subtitle}>Unlimited events, special guest and more.</Typography>
-        <br />
+        <Box className={classes.titleContainer}>
+          <Typography className={classes.title}>Watch Party</Typography>
+          <Typography className={classes.subtitle}>Unlimited events, special guest and more.</Typography>
+        </Box>
         <Typography className={classes.partyCode}>Type Party Code</Typography>
         <Box display="flex">
-          <Input placeholder="Party Code" />
-          <CustomButton buttonType={BUTTONTYPE.PRIMARY}>Join</CustomButton>
+          <Input placeholder="Party Code" className={classes.input} />
+          <CustomButton size={BUTTONSIZE.SMALL} buttonType={BUTTONTYPE.PRIMARY}>
+            Join
+          </CustomButton>
         </Box>
-        <Typography>
-          Already have a party?<Box component="span">Sign in here</Box>
-        </Typography>
+        <Box mt={4} display="flex">
+          <Typography mr={2}>Already have a party?</Typography>
+          <Link to="" className={classes.link}>
+            Sign in here
+          </Link>
+        </Box>
       </Box>
-      <Box width="40%"></Box>
-      {/* <nav>
-        <Link to="/">Home</Link> | <Link to="about">About</Link>
-      </nav>
-
-      <Link to="create">Initiate a new Conference</Link>
-      <Link to="red5pro">Red 5 Event</Link> */}
+      <Box className={classes.rightContainer}>
+        <Box className={classes.imageContainer}>
+          <img
+            className={classes.image}
+            alt="Landing Page Image"
+            src={require('../../assets/images/LandingPageImage.png')}
+          ></img>
+        </Box>
+      </Box>
     </Box>
   )
 }
