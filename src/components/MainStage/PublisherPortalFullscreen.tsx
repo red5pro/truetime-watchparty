fd4dbf61ca3045e557e4470965548f36041cb10f
Todd Anderson <bustardcelly@gmail.com>
2022-08-24 16:07:04 -0400

    grid looks nice on chrome at least.

--- src/components/MainStage/PublisherPortalFullscreen.tsx
+++ src/components/MainStage/PublisherPortalFullscreen.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Box } from '@mui/material'
 import * as portals from 'react-reverse-portal'
 
 interface Props {
@@ -8,16 +7,8 @@ interface Props {
 
 const PublisherPortalFullscreen = (props: Props) => {
   const { portalNode } = props
-  const divRef = React.useRef()
-
-  // React.useEffect(() => {
-  //   if (divRef && divRef.current) {
-  //     ;((divRef.current as Node)?.parentNode as HTMLElement).style.height = '100%'
-  //   }
-  // }, [divRef])
-
   return (
-    <div style={{ display: 'inline-grid' }}>
+    <div style={{ display: '-webkit-inline-box' }}>
       <portals.OutPortal node={portalNode} style={{ height: '100%' }} />
     </div>
   )
