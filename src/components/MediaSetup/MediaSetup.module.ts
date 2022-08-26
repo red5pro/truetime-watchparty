5345e4237cf54c3f50a3901d0301a300aa3a03cb
Todd Anderson <bustardcelly@gmail.com>
2022-08-26 10:50:07 -0400

    some layout changes to join pages.

--- src/components/MediaSetup/MediaSetup.module.ts
+++ src/components/MediaSetup/MediaSetup.module.ts
@@ -8,7 +8,6 @@ const useMediaStyles = makeStyles()({
   },
   video: {
     borderRadius: '20px',
-    width: 'calc(100vw / 3)',
     height: 'calc(100vh / 3)',
     minHeight: '360px',
     backgroundColor: 'black',
