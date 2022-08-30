9eadbd99b06a52b400fb3dae57804941507d7a1b
Todd Anderson <bustardcelly@gmail.com>
2022-08-30 10:43:52 -0400

    using view context for start party.

--- src/components/HostAPartyFlow/StartParty/StartParty.tsx
+++ src/components/HostAPartyFlow/StartParty/StartParty.tsx
@@ -1,55 +1,74 @@
+import React from 'react'
 import { Box, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { AccountCredentials } from '../../../models/AccountCredentials'
 import { ConferenceDetails } from '../../../models/ConferenceDetails'
-import { IStepActionsSubComponent } from '../../../utils/commonUtils'
+import { getStartTimeFromTimestamp, IStepActionsSubComponent } from '../../../utils/commonUtils'
+import ViewContext from '../../ViewContext/ViewContext'
 import SetupPartyForm from './SetupPartyForm'
 import useStyles from './StartParty.module'
+import SimpleAlertDialog from '../../Modal/SimpleAlertDialog'
+import Loading from '../../Loading/Loading'
+
+const useViewContext = () => React.useContext(ViewContext.Context)
 
 interface IStartPartyProps {
   onActions: IStepActionsSubComponent
   data?: ConferenceDetails
   setData: (values: ConferenceDetails) => void
-  currentEpisode: any
-  currentSerie: any
   account: AccountCredentials
 }
 
 const StartParty = (props: IStartPartyProps) => {
-  const { onActions, currentEpisode, currentSerie, data, setData, account } = props
+  const { onActions, data, setData, account } = props
 
   const { classes } = useStyles()
-  const dateOptions: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' }
-  const startDate = new Date(currentEpisode?.startTime ?? '')
+
+  const { loading, error, eventData } = useViewContext()
+
+  const onRetryRequest = () => {
+    window.location.reload()
+    return false
+  }
 
   return (
     <Box className={classes.root}>
       <Typography paddingTop={2} sx={{ textAlign: 'center', fontSize: '16px' }}>
-        {currentEpisode?.displayName}
+        Host a WatchParty
       </Typography>
-      <Box className={classes.container} display="flex" flexDirection="column">
-        <Typography sx={{ fontSize: '24px' }}>{currentSerie.displayName}</Typography>
-        <Typography variant="h1">{currentEpisode?.displayName}</Typography>
-        <Typography
-          variant="h4"
-          marginY={3}
-          sx={{ fontSize: '17px', fontWeight: 600 }}
-        >{`${startDate.toLocaleDateString(
-          'en-US',
-          dateOptions
-        )} - ${startDate.getHours()}:${startDate.getMinutes()}`}</Typography>
-
-        <Box display="flex">
-          <Typography marginRight={2}>Set up your party</Typography>
-
-          {!account && (
-            <Link to="login" className={classes.link}>
-              Returning host?
-            </Link>
-          )}
+      {loading && (
+        <Box className={classes.container} display="flex" alignItems="center">
+          <Loading text="Loading Event Data..." />
         </Box>
-        <SetupPartyForm onActions={onActions} data={data} setData={setData} account={account} />
-      </Box>
+      )}
+      {error && (
+        <SimpleAlertDialog
+          title="Could not load event information"
+          message={`${error.status} - ${error.statusText}`}
+          confirmLabel="Retry"
+          onConfirm={onRetryRequest}
+        />
+      )}
+      {!loading && eventData.loaded && (
+        <Box className={classes.container} display="flex" flexDirection="column">
+          <Typography sx={{ fontSize: '24px' }}>{eventData.currentSeries?.displayName}</Typography>
+          <Typography variant="h2">{eventData.currentEpisode?.displayName}</Typography>
+          <Typography variant="h4" marginY={3} sx={{ fontSize: '18px', fontWeight: 600 }}>
+            {getStartTimeFromTimestamp(eventData.currentEpisode?.startTime)}
+          </Typography>
+
+          <Box display="flex">
+            <Typography marginRight={2}>Set up your party</Typography>
+
+            {!account && (
+              <Link to="login" className={classes.link}>
+                Returning host?
+              </Link>
+            )}
+          </Box>
+          <SetupPartyForm onActions={onActions} data={data} setData={setData} account={account} />
+        </Box>
+      )}
     </Box>
   )
 }
