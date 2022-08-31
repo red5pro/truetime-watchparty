4547cdcbbab4fe45bdd2309de3bdf9b957d343ae
Todd Anderson <bustardcelly@gmail.com>
2022-08-31 09:10:26 -0400

    some layout and styles.

--- src/pages/JoinPage/JoinPage.module.ts
+++ src/pages/JoinPage/JoinPage.module.ts
@@ -5,6 +5,8 @@ const useStyles = makeStyles()((theme: any) => {
     root: {
       // background: 'linear-gradient(to bottom right, #1b1828,#2a448a)',
       backdropFilter: 'blur(88px)',
+      backgroundImage:
+        'radial-gradient(circle at right, rgba(255,0,0, 0.2) 0%, rgba(0, 0, 0, 1) 70%,rgba(0, 0, 1) 100%)',
       width: '100%',
       height: '100%',
       overflow: 'auto',
