1474667fab31a178fc4371a6849a0fe43acb2b08
Todd Anderson <bustardcelly@gmail.com>
2022-09-06 10:29:09 -0400

    redirects for signin from other pages.

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,13 +1,32 @@
-import { useLocation } from 'react-router-dom'
+import React from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import Signin from '../../components/Account/Signin/Signin'
-import { parseQueryParamToObject } from '../../utils/commonUtils'
+import useQueryParams from '../../hooks/useQueryParams'
+import { IStepActionsSubComponent, parseQueryParamToObject } from '../../utils/commonUtils'
 
 const SignInPage = () => {
   const location = useLocation()
   const search = location.search
   const params = search ? (parseQueryParamToObject(search) as any) : {}
+  const query = useQueryParams()
+  const navigate = useNavigate()
 
-  return <Signin emailSignin={params.spg ?? false} />
+  const getLink = (link: string | null) => {
+    return link === 'home' ? `/` : `/${link}`
+  }
+
+  const onActions = query.get('r_id')
+    ? ({
+        onNextStep: () => {
+          navigate(getLink(query.get('r_id')))
+        },
+        onBackStep: () => {
+          navigate(getLink(query.get('r_id')))
+        },
+      } as IStepActionsSubComponent)
+    : undefined
+
+  return <Signin onActions={onActions} emailSignin={params.spg ?? false} />
 }
 
 export default SignInPage
