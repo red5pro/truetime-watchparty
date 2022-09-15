f7455ae4b0d227bfc2782ae9862c72097defe085
LourdesBl <bloin.lourdes@gmail.com>
2022-09-15 14:37:17 -0300

    feature/WAT-96/admin-live-stats

---
+++ src/components/Admin/TabsSection/TabSection.module.ts
@@ -0,0 +1,47 @@
+import { makeStyles } from 'tss-react/mui'
+
+const useStyles = makeStyles()((theme: any) => ({
+  root: {
+    '& .MuiTabs-indicator': {
+      display: 'none',
+    },
+  },
+  tabs: {
+    border: '1px solid #676161',
+    width: 'fit-content',
+    borderRadius: '50px',
+  },
+  currTab: {
+    borderRadius: '20px',
+    backgroundColor: 'white',
+    color: 'black !important',
+  },
+  nonCurrTab: {
+    color: 'white',
+  },
+  container: {
+    paddingBottom: '16px !important',
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    alignContent: 'flex-start',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    height: '100%',
+    border: '1px solid #676161',
+    borderRadius: '16px',
+  },
+  cardText: {
+    fontWeight: '600',
+    lineHeight: '20px',
+    display: 'flex',
+    alignItems: 'center',
+    color: '#FBFBFB',
+    flex: 'none',
+    order: '0',
+    alignSelf: 'stretch',
+    flexGrow: '0',
+  },
+}))
+
+export default useStyles
---
+++ src/models/ConferenceStats.ts
@@ -0,0 +1,33 @@
+export interface AllConferenceStats {
+  avgParticipants: number
+  avgViewTimeS: number
+  curConferences: number
+  curParticipants: number
+  devParticipants: number
+  devViewTimeS: number
+  maxConferences: number
+  maxParticipants: number
+  maxViewTimeS: number
+  totalConferences: number
+  totalParticipants: number
+  totalViewTimeS: number
+}
+
+export interface StatsByConference {
+  conferenceId: number
+  episodeId: number
+  streamGuid: string
+  displayName: string
+  vipOkay: boolean
+  vipVisited: boolean
+  startTimeMs: number
+  endTimeMs: number
+  numKicked: number
+  maxParticipants: number
+  totalParticipants: number
+  maxViewTimeS: number
+  totalViewTimeS: number
+  avgViewTimeS: number
+  devViewTimeS: number
+  curParticipants: number
+}
