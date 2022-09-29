9e0b135c5024e04e678a6d00b1d713994b63d977
Todd Anderson <bustardcelly@gmail.com>
2022-09-29 10:22:27 -0400

    changing place where version is printed to console.

--- src/index.js
+++ src/index.js
@@ -24,11 +24,10 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 )
+console.log(`Watch Party Version: ${process.env.REACT_APP_VERSION}`)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
 module.hot.accept()
-
-console.log(`Watch Party Version: ${process.env.REACT_APP_VERSION}`)
