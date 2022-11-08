36472a34096b8e677c603e21937702db19e0779e
LourdesBl <bloin.lourdes@gmail.com>
2022-11-08 11:03:06 -0300

    bug/WAT-198

--- src/utils/accountUtils.ts
+++ src/utils/accountUtils.ts
@@ -2,13 +2,13 @@ import * as Yup from 'yup'
 
 const testPassword = (value?: string) => {
   if (value) {
-    return /^(?=[^A-Z\n]*[A-Z])(?=[^a-z\n]*[a-z])(?=[^0-9\n]*[0-9]).{6,}$/.test(value)
+    return /^(?=[^A-Z\n]*[A-Z])(?=[^a-z\n]*[a-z])(?=[^0-9\n]*[0-9]).{8,}$/.test(value)
   } else return false
 }
 
 export const validationSchema = Yup.object().shape({
   email: Yup.string().email('Invalid Email').required('Email field is required'),
-  password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password field is required'),
+  password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password field is required'),
   // .test('', 'Password must include at least one number, one uppercase letter and one lowercase letter', (value) =>
   //   testPassword(value)
   // )
