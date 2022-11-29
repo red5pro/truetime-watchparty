ab3fbc13b4589f4c2ef01fadc1251a95ad57425b
LourdesBl <bloin.lourdes@gmail.com>
2022-11-29 14:33:01 -0300

    improve-admin-page-performance

--- src/components/Admin/MainTotalValues/MainTotalValues.tsx
+++ src/components/Admin/MainTotalValues/MainTotalValues.tsx
@@ -1,21 +1,68 @@
 import * as React from 'react'
+
 import Box from '@mui/material/Box'
+
 import CardComponent from './CardComponent'
 import { AllConferenceStats } from '../../../models/ConferenceStats'
+import { STATS_API_CALLS } from '../../../services/api/stats-api-calls'
+import SimpleAlertDialog from '../../Modal/SimpleAlertDialog'
 
 interface IMainTotalValuesProps {
   stats: AllConferenceStats
+  cookies: any
 }
 
-const MainTotalValues = ({ stats }: IMainTotalValuesProps) => {
+const MainTotalValues = ({ cookies, stats }: IMainTotalValuesProps) => {
+  const [error, setError] = React.useState<any>()
+  const [allStats, setAllStats] = React.useState<AllConferenceStats>(stats)
+  const [interval, setInt] = React.useState<any>()
+
+  // Set Interval
+  const action = async () => {
+    const fetchStats = await STATS_API_CALLS.getAllConferenceStats(cookies.account.username, cookies.account.password)
+
+    if (fetchStats.status === 200 && fetchStats.data) {
+      setAllStats(fetchStats.data as AllConferenceStats)
+    } else {
+      setError({
+        status: `Got an error when trying to retrieve all stats.`,
+        statusText: `${fetchStats.statusText}.`,
+      })
+    }
+  }
+
+  React.useEffect(() => {
+    if (cookies) {
+      setInt(setInterval(async () => await action(), 10000))
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    }
+  }, [])
+
   return (
     <Box display="flex" justifyContent="space-around">
-      <CardComponent text="Number of active main feed viewers" value={stats.curParticipants} />
-      <CardComponent text="Number of active conferences" value={stats.curConferences} />
-      <CardComponent text="Average number of participants per conference" value={stats.avgParticipants} />
-      <CardComponent text="Average conference length in seconds" value={stats.avgViewTimeS} />
+      {!error ? (
+        <>
+          <CardComponent text="Number of active main feed viewers" value={allStats.curParticipants} />
+          <CardComponent text="Number of active conferences" value={allStats.curConferences} />
+          <CardComponent text="Average number of participants per conference" value={allStats.avgParticipants} />
+          <CardComponent text="Average conference length in seconds" value={allStats.avgViewTimeS} />
+        </>
+      ) : (
+        <SimpleAlertDialog
+          title="Something went wrong"
+          message={`${error.status} - ${error.statusText}`}
+          confirmLabel="Ok"
+          onConfirm={() => setError(null)}
+        />
+      )}
     </Box>
   )
 }
 
 export default MainTotalValues
+
--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -11,30 +11,22 @@ import Loading from '../../components/Common/Loading/Loading'
 import SimpleAlertDialog from '../../components/Modal/SimpleAlertDialog'
 import useCookies from '../../hooks/useCookies'
 import { AllConferenceStats, StatsByConference } from '../../models/ConferenceStats'
-import { STATS_API_CALLS } from '../../services/api/stats-api-calls'
-import { getStatsByConference } from '../../services/conference/stats'
+import { getStatsByConference } from '../../services/stats'
 import { UserRoles } from '../../utils/commonUtils'
-import { getSortedCountryList } from '../../utils/statsUtils'
 import useStyles from './AdminPage.module'
-import { Serie } from '../../models/Serie'
-import { SERIES_API_CALLS } from '../../services/api/serie-api-calls'
 
 const AdminPage = () => {
-  const [cookies, setCookies] = React.useState<any>()
-  const [ready, setReady] = React.useState<boolean>(false)
   const [loading, setLoading] = React.useState<boolean>(true)
-  const [allStats, setAllStats] = React.useState<AllConferenceStats>()
-  const [statsByConference, setStatsByConference] = React.useState<StatsByConference[]>()
-  const [series, setSeries] = React.useState<Serie[]>([])
-  const [countries, setCountries] = React.useState<{ country: string; count: number }[]>([])
+  const [cookies, setCookies] = React.useState<any>()
   const [error, setError] = React.useState<any>()
   const [openCreatePage, setOpenCreatePage] = React.useState<boolean>(false)
-  const [interval, setInt] = React.useState<any>()
 
   const { getCookies, removeCookie } = useCookies(['userAccount', 'account'])
-
   const navigate = useNavigate()
   const { classes } = useStyles()
+
+  const [statsByConference, setStatsByConference] = React.useState<StatsByConference[]>([])
+  const [allConferenceStats, setAllConferenceStats] = React.useState<AllConferenceStats>({} as AllConferenceStats)
 
   React.useEffect(() => {
     const cookiesUpdated = getCookies()
@@ -43,61 +35,26 @@ const AdminPage = () => {
       removeCookie('userAccount')
       removeCookie('account')
       navigate('/login?r_id=admin')
-      return
+    } else {
+      setCookies(cookiesUpdated)
+      getStats(cookiesUpdated)
     }
-
-    setCookies(cookiesUpdated)
-    setReady(true)
   }, [])
 
-  React.useEffect(() => {
-    if (ready && cookies && cookies.account.username && cookies.account.password) {
-      setInt(setInterval(async () => await getStats(), 10000))
-    }
+  const getStats = async (cookies: any) => {
+    const response = await getStatsByConference(cookies.account.username, cookies.account.password)
 
-    return () => {
-      if (interval) {
-        clearInterval(interval)
-      }
-    }
-  }, [ready])
-
-  const getStats = async () => {
-    const allStats = await STATS_API_CALLS.getAllConferenceStats(cookies.account.username, cookies.account.password)
-    if (allStats.status === 200 && allStats.data) {
-      setAllStats(allStats.data as AllConferenceStats)
+    if (response.status === 200 && response.data) {
+      setStatsByConference(response.data.statsByConferences)
+      setAllConferenceStats(response.data.allConferecesStats)
     } else {
       setError({
-        status: `Got an error when trying to retrieve all stats.`,
-        statusText: `${allStats.statusText}.`,
+        status: `Warning!`,
+        statusText: `${response.statusText}`,
       })
     }
-
-    const statsByConference = await getStatsByConference(cookies.account.username, cookies.account.password)
-
-    if (statsByConference.status === 200 && statsByConference.data) {
-      setStatsByConference(statsByConference.data)
-
-      setCountries(getSortedCountryList(statsByConference.data))
-    } else {
-      setError({
-        status: `Got an error when trying to retrieve conference stats.`,
-        statusText: `${statsByConference.statusText}.`,
-      })
-    }
-
-    await getSeries()
 
     setLoading(false)
-    setReady(false)
-  }
-
-  const getSeries = async () => {
-    const seriesResponse = await SERIES_API_CALLS.getSeriesList()
-
-    if (seriesResponse.status === 200 && seriesResponse.data?.series) {
-      setSeries(seriesResponse.data.series)
-    }
   }
 
   return (
@@ -110,24 +67,23 @@ const AdminPage = () => {
           Watch Party Admin
         </Typography>
       </Box>
+      {loading && <Loading />}
 
-      {allStats && !openCreatePage && <MainTotalValues stats={allStats} />}
-      {statsByConference && (
+      {!loading && !openCreatePage && cookies && allConferenceStats && (
+        <MainTotalValues stats={allConferenceStats} cookies={cookies} />
+      )}
+
+      {!loading && cookies && statsByConference && (
         <LocalizationProvider dateAdapter={AdapterMoment}>
           <TabsSection
-            statsByConferece={statsByConference}
-            countries={countries}
+            cookies={cookies}
+            statsByConference={statsByConference}
             setError={setError}
-            setLoading={setLoading}
             setOpenCreatePage={setOpenCreatePage}
             openCreatePage={openCreatePage}
-            setReady={setReady}
-            series={series}
-            getSeries={getSeries}
           />
         </LocalizationProvider>
       )}
-      {((!allStats && loading) || (!statsByConference && allStats)) && <Loading />}
 
       {error && (
         <SimpleAlertDialog
@@ -142,3 +98,4 @@ const AdminPage = () => {
 }
 
 export default AdminPage
+
