4e9ee13fd4cab5c7d13ebab1d172c329bc1b203e
LourdesBl <bloin.lourdes@gmail.com>
2023-01-23 12:49:08 -0300

    improvements

--- src/services/stats.ts
+++ src/services/stats.ts
@@ -9,53 +9,11 @@ export const getStatsByConference = async (user: string, password: string) => {
     email: user,
     password: password,
   }
-  const allConferencesResponse = await CONFERENCE_API_CALLS.getAllConferences(account)
-
-  if (allConferencesResponse.status === 200 && allConferencesResponse.data.conferences.length) {
-    const statsByConferences: StatsByConference[] = []
-    const results = []
-
-    for (let i = 0; i < allConferencesResponse.data.conferences.length; i++) {
-      const response = await STATS_API_CALLS.getStatsByConference(
-        user,
-        password,
-        allConferencesResponse.data.conferences[i].conferenceId
-      )
-
-      if (response.status === 200 && Object.values(response.data).length) {
-        statsByConferences.push({ ...response.data, ...allConferencesResponse.data.conferences[i] })
-        results.push(response)
-      }
-    }
-
-    const allConferecesStatsResponse = await STATS_API_CALLS.getAllConferenceStats(user, password)
-
-    if (allConferecesStatsResponse.status === 200 && allConferecesStatsResponse.data) {
-      return {
-        data: { statsByConferences, allConferecesStats: allConferecesStatsResponse.data },
-        status: 200,
-      } as AxiosResponse
-    }
-
-    return {
-      data: { statsByConferences },
-      status: 200,
-    } as AxiosResponse
-  } else {
-    return allConferencesResponse as AxiosResponse
-  }
-}
-
-export const getStatsByConference1 = async (user: string, password: string) => {
-  const account: AccountCredentials = {
-    email: user,
-    password: password,
-  }
   const statsByConferences: StatsByConference[] = []
   const allConferencesPromise = CONFERENCE_API_CALLS.getAllConferences(account)
   const allConferecesStatsPromise = STATS_API_CALLS.getAllConferenceStats(user, password)
 
-  Promise.allSettled([allConferencesPromise, allConferecesStatsPromise])
+  return Promise.allSettled([allConferencesPromise, allConferecesStatsPromise])
     .then(async (results) => {
       if (results[0].status === 'fulfilled') {
         const allConferencesResponse = results[0].value
