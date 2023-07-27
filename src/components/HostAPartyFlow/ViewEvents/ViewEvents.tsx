bd5f85b4c5afdff70f0acc240b0a0013690ce563
Todd Anderson <bustardcelly@gmail.com>
2023-07-27 10:03:31 -0400

    cherry-pick series event listing.

--- src/components/HostAPartyFlow/ViewEvents/ViewEvents.tsx
+++ src/components/HostAPartyFlow/ViewEvents/ViewEvents.tsx
@@ -111,10 +111,10 @@ const ViewEvents = (props: IViewEventsProps) => {
               </>
             )}
           </Box>
-          {eventData.nextEpisodes?.length > 0 && (
+          {eventData.loaded && eventData.nextEpisodes?.length > 0 && (
             <Box className={classes.rightContainer}>
               <Typography sx={{ fontWeight: 600, textAlign: 'left' }} marginBottom={2}>
-                Other events in this tournament
+                Other events in this series
               </Typography>
               <ElementList items={eventData.nextEpisodes} />
             </Box>
