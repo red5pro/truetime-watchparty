9942fb660749b559a7e55c251a05b585a020377f
Todd Anderson <bustardcelly@gmail.com>
2023-07-27 15:53:25 -0400

    toggle video mute update.

--- src/components/SubscriberMenu/SubscriberMenu.tsx
+++ src/components/SubscriberMenu/SubscriberMenu.tsx
@@ -76,7 +76,7 @@ const SubscriberMenu = (props: SubscriberMenuProps) => {
   }
 
   const toggleMuteVideo = () => {
-    const muted = participant.muteState?.audioMuted
+    const muted = participant.muteState?.videoMuted
     actions?.onMuteVideo(participant, !muted)
     onToggleMenu()
   }
