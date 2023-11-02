d42241cb026dc4b52c1017a1285b8fc23a8a57fb
Todd Anderson <bustardcelly@gmail.com>
2023-11-02 10:21:09 -0400

    target out path for modes.

--- scripts/define-env-mode.mjs
+++ scripts/define-env-mode.mjs
@@ -8,11 +8,17 @@ const inject = async () => {
     from: /REACT_APP_WEBAPP_MODE=.*/g,
     to: `REACT_APP_WEBAPP_MODE="${mode}"`,
   }
+  const output = {
+    files: './.env.example',
+    from: /BUILD_PATH=.*/g,
+    to: `BUILD_PATH="/${mode}"`,
+  }
 
   console.log(`Defining mode: ${options.to}`)
   try {
     const results = await replace(options)
-    console.log('Replacement results:', results)
+    const outResults = await replace(output)
+    console.log('Replacement results:', results, outResults)
   } catch (error) {
     console.error('Error occurred:', error)
   }
